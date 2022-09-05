fbc9db355bf2f1f7493f290b3eefaea1f2e45103
Ahmed Bargady <bargadyahmed2000@gmail.com>
2022-09-05 20:20:03 +0100

    adding sitemap

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,6 +43,13 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
+
+        sitemap: {
+          changefreq: "weekly",
+          priority: 0.5,
+          ignorePatterns: ["/tags/**"],
+          filename: "sitemap.xml",
+        },
       }),
     ],
   ],
