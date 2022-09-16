f97f4dc4056191c5fa6e6b1a89ca7991f72cf7a1
AhmedCoolProjects <bargadyahmed2000@gmail.com>
2022-09-16 21:45:21 +0100

    working

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/docs">
+          <Link className="button button--secondary button--lg" to="/api">
             Get Started With Market Data's API - 1min ⏱️
           </Link>
         </div>
