0f42ee7b28c3bac9f3c7475972a134640c049eee
AhmedCoolProjects <bargadyahmed2000@gmail.com>
2024-01-25 21:53:09 +0100

    fix: base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
       ? "https://docs.marketdata.app/"
       : "https://docs-staging.marketdata.app/",
 
-  baseUrl: "/",
+  baseUrl: "/docs-staging/",
   onBrokenLinks: "ignore",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
