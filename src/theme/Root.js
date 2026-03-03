0caef7087f2953e96f2bd2b5cc22f30de3d1dad8
MarketDataApp <github@cdnhq.net>
2026-03-03 15:40:45 -0300

    fix: skip Cloudflare Zaraz script in development
    /cdn-cgi/zaraz/i.js only exists on the live Cloudflare site. In dev,
    the Webpack dev server returns an HTML 404 for that path, which throws
    "Unexpected token '<'" when the browser tries to execute it as JS.

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -2,12 +2,16 @@ import React from 'react';
 import Head from '@docusaurus/Head';
 import Context7Widget from '@site/src/components/Context7Widget';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export default function Root({children}) {
   return (
     <>
-      <Head>
-        <script data-cfasync="false" src="/cdn-cgi/zaraz/i.js" referrerPolicy="origin" />
-      </Head>
+      {isProd && (
+        <Head>
+          <script data-cfasync="false" src="/cdn-cgi/zaraz/i.js" referrerPolicy="origin" />
+        </Head>
+      )}
       {children}
       <Context7Widget />
     </>
